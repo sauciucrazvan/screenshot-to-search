9166822a074b7a9dfe6185dfc226d3bdccc51bf2
Razvan <razvansau9@gmail.com>
2024-04-15 04:48:58 +0300

    readded runner

---
+++ runner/runner.py
@@ -0,0 +1,18 @@
+import keyboard, time, subprocess
+
+def main():
+
+    started = False
+
+    while True:
+        if keyboard.is_pressed("ctrl+shift+j"):
+            if not started:
+                subprocess.Popen("app.exe")
+                started = True
+        else:
+            started = False
+        
+        time.sleep(0.1)
+
+if __name__ == "__main__":
+    main()
---
+++ screenshot-to-search.py
@@ -0,0 +1,4 @@
+from runner import runner
+
+if __name__ == "__main__":
+    runner.main()
