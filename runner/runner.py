ed626c4a9d84a117475158e336abef7fa31780ca
Razvan <razvansau9@gmail.com>
2024-04-19 14:17:46 +0300

    added a config.yaml to change the default shortcut

--- runner/runner.py
+++ runner/runner.py
@@ -1,18 +1,23 @@
-import keyboard, time, subprocess
+import keyboard, time, subprocess, yaml
 
 def main():
 
     started = False
 
-    while True:
-        if keyboard.is_pressed("ctrl+shift+j"):
-            if not started:
-                subprocess.Popen("bin/app.exe")
-                started = True
-        else:
-            started = False
-        
-        time.sleep(0.1)
+    with open("config.yaml", "r") as f:
+        data = yaml.safe_load(f)
+
+        shortcut = data["shortcut"]
+
+        while True:
+            if keyboard.is_pressed(shortcut):
+                if not started:
+                    subprocess.Popen("bin/app.exe")
+                    started = True
+            else:
+                started = False
+            
+            time.sleep(0.1)
 
 if __name__ == "__main__":
     main()
